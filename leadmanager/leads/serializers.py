3e966df6f254a2a3f4d12870dae5080c757d881f
Amin <asaa47721@gmail.com>
2019-11-10 16:11:06 +0330

    add Pipfile, contains installed pkgs

---
+++ leadmanager/leads/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Lead
+
+
+class LeadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lead
+        fields = [
+            'name',
+            'email',
+            'message',
+            'created_at',
+        ]

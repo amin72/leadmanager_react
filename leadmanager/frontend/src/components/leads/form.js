d5709e7911501e341883dffa2f5c1e56e3052174
Amin <asaa47721@gmail.com>
2019-11-12 18:55:44 +0330

    add lead form in form.js

--- leadmanager/frontend/src/components/leads/form.js
+++ leadmanager/frontend/src/components/leads/form.js
@@ -2,10 +2,72 @@ import React, {Component} from 'react'
 
 
 export default class Form extends Component {
+    state = {
+        name: '',
+        email: '',
+        message: ''
+    }
+
+
+    onChange = e => {
+        this.setState({[e.target.name]: e.target.value})
+    }
+
+
+    onSubmit = e => {
+        e.preventDefault()
+        console.log('submit')
+    }
+
+
     render() {
+        const {name, email, message} = this.state
+
         return (
-            <div>
-                <h1>Add Lead Form</h1>
+            <div className="card card-body mt-4 mb-4">
+                <h2>Add Lead</h2>
+                
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Name</label>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="name"
+                            onChange={this.onChange}
+                            value={name}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input
+                            className="form-control"
+                            type="email"
+                            name="email"
+                            onChange={this.onChange}
+                            value={email}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Message</label>
+                        <textarea
+                            className="form-control"
+                            type="text"
+                            name="message"
+                            onChange={this.onChange}
+                            value={message}
+                            required
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary">Submit</button>
+                    </div>
+                </form>
             </div>
         )
     }

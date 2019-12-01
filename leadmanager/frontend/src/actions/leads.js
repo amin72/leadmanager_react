c198ec0d212082a255af51a56234214532bdb0fa
Amin <asaa47721@gmail.com>
2019-12-01 20:15:11 +0330

    add returned errors to redux

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,26 +1,17 @@
 import axios from 'axios'
 import {GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types'
-import { createMessage } from './messages'
+import { createMessage, returnErrors } from './messages'
 
 
 // GET LEADS
-export const getLeads = () => dispath => {
+export const getLeads = () => dispatch => {
     axios.get('/api/leads/')
         .then(res => {
-            dispath({
+            dispatch({
                 type: GET_LEADS,
                 payload: res.data
             })
-        }).catch(err => {
-            const errors = {
-                msg: err.response.data,
-                status: err.response.status
-            }
-            dispatch({
-                type: GET_ERRORS,
-                payload: errors
-            })
-        })
+        }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
 }
 
 
@@ -59,14 +50,5 @@ export const addLead = (lead) => dispatch => {
                 payload: res.data
             })
 
-        }).catch(err => {
-            const errors = {
-                msg: err.response.data,
-                status: err.response.status
-            }
-            dispatch({
-                type: GET_ERRORS,
-                payload: errors
-            })
-        })
+        }).catch(err => dispatch(returnErrors(err.response.data, err.response.status)))
 }
--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,4 +1,4 @@
-import { CREATE_MESSAGE } from "./types"
+import { CREATE_MESSAGE, GET_ERRORS } from "./types"
 
 
 // Create message
@@ -6,5 +6,14 @@ export const createMessage = msg => {
     return {
         type: CREATE_MESSAGE,
         payload: msg
+    }
+}
+
+
+// Return errors
+export const returnErrors = (msg, status) => {
+    return {
+        type: GET_ERRORS,
+        payload: { msg, status }
     }
 }

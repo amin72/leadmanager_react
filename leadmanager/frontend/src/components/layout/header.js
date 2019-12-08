6274fc7e7a168445315fcfaa115b358785d49f19
Amin <asaa47721@gmail.com>
2019-12-08 20:54:56 +0330

    add welcome message to logged in user in Header component

--- leadmanager/frontend/src/components/layout/header.js
+++ leadmanager/frontend/src/components/layout/header.js
@@ -28,6 +28,11 @@ class Header extends Component {
 
         const guestLinks = (
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+                <span className="navbar-text mr-3">
+                    <strong>
+                        { user ? `Welcome ${user.username}` : "" }
+                    </strong>
+                </span>
                 <li className="nav-item">
                     <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">Logout</button>
                 </li>

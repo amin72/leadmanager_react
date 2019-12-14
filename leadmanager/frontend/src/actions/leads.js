64b96438952bb4bfb1cc248ae0b1a314da507627
Amin <asaa47721@gmail.com>
2019-12-14 18:06:13 +0330

    add tokenConfig to leads actions. leads belongs to specific user now

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,11 +1,12 @@
 import axios from 'axios'
 import {GET_LEADS, DELETE_LEAD, ADD_LEAD, GET_ERRORS} from './types'
 import { createMessage, returnErrors } from './messages'
+import { tokenConfig } from './auth'
 
 
 // GET LEADS
-export const getLeads = () => dispatch => {
-    axios.get('/api/leads/')
+export const getLeads = () => (dispatch, getState) => {
+    axios.get('/api/leads/', tokenConfig(getState))
         .then(res => {
             dispatch({
                 type: GET_LEADS,
@@ -16,8 +17,8 @@ export const getLeads = () => dispatch => {
 
 
 // DELETE LEAD
-export const deleteLead = (id) => dispatch => {
-    axios.delete(`/api/leads/${id}/`)
+export const deleteLead = (id) => (dispatch, getState) => {
+    axios.delete(`/api/leads/${id}/`, tokenConfig(getState))
         .then(res => {
             dispatch(createMessage({ deleteLead: 'Lead Deleted' }))
 
@@ -40,8 +41,8 @@ export const deleteLead = (id) => dispatch => {
 
 
 // ADD LEAD
-export const addLead = (lead) => dispatch => {
-    axios.post('/api/leads/', lead)
+export const addLead = (lead) => (dispatch, getState) => {
+    axios.post('/api/leads/', lead, tokenConfig(getState))
         .then(res => {
             dispatch(createMessage({ addLead: 'Lead Added' }))
 
